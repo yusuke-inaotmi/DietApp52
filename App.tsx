1ea461f6cbacc6eb3f952c5c38422da813c229e3
yusuke-inatomi <yusuke.inatomi@gmail.com>
2025-01-11 20:43:29 +0900

    Create Hello components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import Hello from './src/components/Hello'
+
 const App = (): JSX.Element => {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello style={{ fontSize: 16}}>World</Hello>
       <Text>Open up App.tsx to start working on your app!!!!</Text>
       <StatusBar style="auto" />
     </View>
