044fde141f2d570aa66ce26f113b372b08834608
yusuke-inatomi <yusuke.inatomi@gmail.com>
2025-05-04 19:41:16 +0900

    Markup calendar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            // ヘッダ
+            <View>
+                <View>
+                    <Text>カレンダー</Text>
+                    <Text>レベル</Text>
+                    <Text>グラフ</Text>
+                </View>
+            </View>
+            //コンテンツ
+            <View>
+                <View>
+                    <Text>カレンダー</Text>
+                </View>
+                <View>
+                    <Text>サブコンテンツ</Text>
+                </View>
+            </View>
+            //フッタ
+            <View>
+                <View>
+                    <Text>フッタ</Text>
+                </View>
+            </View>
+
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Index
